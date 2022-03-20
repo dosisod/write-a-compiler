d639a619253e27f6cf32279219d5e570f0cfc1b3
dosisod <39638017+dosisod@users.noreply.github.com>
2022-03-20 15:42:28 -0700

    Use new generate_location_info function in tokenizer

--- wac/parse/token.py
+++ wac/parse/token.py
@@ -34,5 +34,33 @@ def generate_location_info(
             column += 1
 
 
+def char_to_token_type(c: str) -> Optional[TokenType]:
+    if c == "\n":
+        return TokenType.NEWLINE
+
+    if c.isspace():
+        return TokenType.WHITESPACE
+
+    if c.isalpha():
+        return TokenType.IDENTIFIER
+
+    return None
+
+
 def tokenize(code: str) -> List[Token]:
-    return [Token(code, 1, 1)]
+    locations = generate_location_info(code)
+
+    token_parts = [next(locations)]
+
+    for location in locations:
+        token_parts.append(location)
+
+    def collapse_token(parts):
+        out = parts.pop(0)
+
+        for part in parts:
+            out = (out[0] + part[0], out[1], out[2])
+
+        return Token(out[0], out[1], out[2])
+
+    return [collapse_token(token_parts)]
