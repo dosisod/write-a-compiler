a03fe9a7f71d532f27d19de87894dc3a10344302
dosisod <39638017+dosisod@users.noreply.github.com>
2022-03-20 17:52:50 -0700

    Add one more test, cleanup existing tests

--- test/test_tokenizer.py
+++ test/test_tokenizer.py
@@ -26,27 +26,35 @@ def test_token_compare():
 def test_generate_location_info():
     locations = list(generate_location_info("a\nbc\ndef"))
 
-    assert len(locations) == 8
-    assert locations[0] == ("a", 1, 1)
-    assert locations[1] == ("\n", 1, 2)
-    assert locations[2] == ("b", 2, 1)
-    assert locations[3] == ("c", 2, 2)
-    assert locations[4] == ("\n", 2, 3)
-    assert locations[5] == ("d", 3, 1)
-    assert locations[6] == ("e", 3, 2)
-    assert locations[7] == ("f", 3, 3)
+    assert locations == [
+        ("a", 1, 1),
+        ("\n", 1, 2),
+        ("b", 2, 1),
+        ("c", 2, 2),
+        ("\n", 2, 3),
+        ("d", 3, 1),
+        ("e", 3, 2),
+        ("f", 3, 3),
+    ]
 
 
 def test_tokenize_single_token():
     tokens = tokenize("hello")
 
-    assert len(tokens) == 1
-    assert tokens[0] == Token("hello", 1, 1)
+    assert tokens == [Token("hello", 1, 1)]
 
 
 def test_tokenize_2_tokens():
     tokens = tokenize("hello\n")
 
-    assert len(tokens) == 2
-    assert tokens[0] == Token("hello", 1, 1)
-    assert tokens[1] == Token("\n", 1, 6)
+    assert tokens == [Token("hello", 1, 1), Token("\n", 1, 6)]
+
+
+def test_tokenize_many_tokens():
+    tokens = tokenize("hello world")
+
+    assert tokens == [
+        Token("hello", 1, 1),
+        Token(" ", 1, 6),
+        Token("world", 1, 7),
+    ]
